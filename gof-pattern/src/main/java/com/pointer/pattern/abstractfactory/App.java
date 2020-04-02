0567eb2ca7bb42080c22a8d3e69a069372f2ea9b
Volodymyr00 <istoria1918@gmail.com>
2020-04-02 13:28:11 +0300

    Karavan Factory

--- gof-pattern/src/main/java/com/pointer/pattern/abstractfactory/App.java
+++ gof-pattern/src/main/java/com/pointer/pattern/abstractfactory/App.java
@@ -4,15 +4,16 @@ package com.pointer.pattern.abstractfactory;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String... args) {
         AbstractFactory abstractFactory = null;
         if ("cola".equals(args[0])) {
             abstractFactory = new CocaColaFactory();
-        }else if("fanta".equals(args[0])){
-            abstractFactory = new FantaFactory();
-        }
+        } else if ("karavan".equals(args[0])) {
+            abstractFactory = new KaravanFactory();
 
-        Client client = new Client(abstractFactory);
-        client.run();
+            Client client = new Client(abstractFactory);
+            client.run();
+        }
     }
 }
---
+++ gof-pattern/src/main/java/com/pointer/pattern/abstractfactory/KaravanBottle.java
@@ -0,0 +1,8 @@
+package com.pointer.pattern.abstractfactory;
+
+public class KaravanBottle extends Bottle {
+    @Override
+    int fill(Water water) {
+        return 1;
+    }
+}
